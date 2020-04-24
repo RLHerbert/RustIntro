50fa46548d5da6496b9eee5201a5f5af4f8695af
RhymesWithCow <37909949+RhymesWithCow@users.noreply.github.com>
2020-04-24 16:43:36 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use ferris_says::say; // Import the say function from the ferrist_says "crate"
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    let stdout = stdout();
+    let out = b"Hello fellow Rustaceans!";
+    let width = 24;
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(out, width, &mut writer).unwrap();
+}
